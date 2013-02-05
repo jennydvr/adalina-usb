597b49243ac74a185c7a5da6077a1f1aa39b8aeb
Jenny Valdez <jedaniv@gmail.com>
2013-02-05 15:27:28 -0430

    Clase neurona

---
+++ adalina-usb/adalina-usb/neuron.cpp
@@ -0,0 +1,9 @@
+//
+//  neuron.cpp
+//  adalina-usb
+//
+//  Created by Jenny Valdez on 05/02/13.
+//  Copyright (c) 2013 Jenny Valdez. All rights reserved.
+//
+
+#include "neuron.h"
---
+++ adalina-usb/adalina-usb/neuron.h
@@ -0,0 +1,14 @@
+//
+//  neuron.h
+//  adalina-usb
+//
+//  Created by Jenny Valdez on 05/02/13.
+//  Copyright (c) 2013 Jenny Valdez. All rights reserved.
+//
+
+#ifndef __adalina_usb__neuron__
+#define __adalina_usb__neuron__
+
+#include <iostream>
+
+#endif /* defined(__adalina_usb__neuron__) */
