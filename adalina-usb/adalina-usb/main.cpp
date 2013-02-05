f455d81279268d123380b707bc8b5740d46e1d17
Jenny Valdez <jedaniv@gmail.com>
2013-02-05 14:10:35 -0430

    Primer commit

---
+++ adalina-usb/adalina-usb/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  adalina-usb
+//
+//  Created by Jenny Valdez on 05/02/13.
+//  Copyright (c) 2013 Jenny Valdez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
